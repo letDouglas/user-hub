060f033d37ed11144b3418156b71e00e39683d77
letDouglas <borsettidamirdouglas@gmail.com>
2025-02-21 14:05:13 +0100

    Init project

---
+++ user-hub-api/src/main/java/novamor/userhubapi/UserHubApiApplication.java
@@ -0,0 +1,13 @@
+package novamor.userhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserHubApiApplication.class, args);
+    }
+
+}
---
+++ user-hub-api/src/test/java/novamor/userhubapi/UserHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package novamor.userhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
